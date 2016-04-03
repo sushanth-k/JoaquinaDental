09e73378bbf104fe3f61bcd299b36425da23c8c9
Sushanth Kothapally <sushanth5588@gmail.com>
2016-04-02 19:46:41 -0500

    Initial Commit

---
+++ src/ToDoListAPI/Controllers/ToDoListController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ToDoListAPI.Filters;
+using System.Net.Http.Headers;
+using System.Configuration;
+using ToDoListAPI.Models;
+using ToDoListAPI;
+using ToDoListAPI.Data_Connect;
+using System.Data.SqlClient;
+
+namespace ToDoListAPI.Filters
+{
+    using System.Web.Http.Filters;
+
+    public class HttpOperationExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            if (context.Exception is Microsoft.Rest.HttpOperationException)
+            {
+                var ex = (Microsoft.Rest.HttpOperationException)context.Exception;
+                context.Response = ex.Response;
+            }
+        }
+    }
+}
+
+namespace ToDoListAPI.Controllers
+{
+    [HttpOperationExceptionFilterAttribute]
+    public class ToDoListController : ApiController
+    {
+        private string owner = "*";
+
+        private static ToDoListDataAPI NewDataAPIClient()
+        {
+            var client = new ToDoListDataAPI(new Uri(ConfigurationManager.AppSettings["toDoListDataAPIURL"]));
+            // Uncomment following line and entire ServicePrincipal.cs file for service principal authentication of calls to ToDoListDataAPI
+            //client.HttpClient.DefaultRequestHeaders.Authorization =
+            //    new AuthenticationHeaderValue("Bearer", ServicePrincipal.GetS2SAccessTokenForProdMSA().AccessToken);
+            return client;
+        }
+
+        // GET: api/ToDoItemList
+        public async Task<IEnumerable<ToDoItem>> Get()
+        {
+            // Uncomment following line in each action method for user authentication
+            //owner = ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value;
+            DBUtility newUtil = new DBUtility();
+            var myConnection = newUtil.CreateConnection();
+            var dbName = myConnection.Database;
+            myConnection.Open();
+            SqlCommand testCommand = new SqlCommand();
+            testCommand.Connection = myConnection;
+            testCommand.CommandText = "select * from dentist";
+            var output = testCommand.ExecuteNonQuery();
+
+
+            //sqlConnection.CreateCommand()
+
+
+            using (var client = NewDataAPIClient())
+            {
+                //var results = await client.ToDoList.GetByOwnerAsync(owner);
+                var results = new List<ToDoItem>();
+                ToDoItem testItem = new ToDoItem();
+                testItem.ID = 12455;
+                testItem.Description = "testing";
+                ToDoItem testItem2 = new ToDoItem();
+                testItem2.ID = 125;
+                testItem2.Description = "validating";
+
+                results.Add(testItem);
+                return results.Select(m => new ToDoItem
+                {
+                    Description = m.Description,
+                    ID = (int)m.ID,
+                    Owner = m.Owner
+                });
+            }
+        }
+
+        // GET: api/ToDoItemList/5
+        public async Task<ToDoItem> GetByID(int id)
+        {
+            //owner = ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value;
+            using (var client = NewDataAPIClient())
+            {
+                var result = await client.ToDoList.GetByIdByOwnerAndIdAsync(owner, id);
+                return new ToDoItem
+                {
+                    Description = result.Description,
+                    ID = (int)result.ID,
+                    Owner = result.Owner
+                };
+            }
+        }
+
+        // POST: api/ToDoItemList
+        public async Task Post(ToDoItem todo)
+        {
+            //owner = ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value;
+            todo.Owner = owner;
+            using (var client = NewDataAPIClient())
+            {
+                await client.ToDoList.PostByTodoAsync(new ToDoItem
+                {
+                    Description = todo.Description,
+                    ID = todo.ID,
+                    Owner = todo.Owner
+                });
+            }
+        }
+
+        public async Task Put(ToDoItem todo)
+        {
+            //owner = ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value;
+            todo.Owner = owner;
+            using (var client = NewDataAPIClient())
+            {
+                await client.ToDoList.PutByTodoAsync(new ToDoItem
+                {
+                    Description = todo.Description,
+                    ID = todo.ID,
+                    Owner = todo.Owner
+                });
+            }
+        }
+
+        // DELETE: api/ToDoItemList/5
+        public async Task Delete(int id)
+        {
+            //owner = ((ClaimsIdentity)User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value;
+            using (var client = NewDataAPIClient())
+            {
+                await client.ToDoList.DeleteByOwnerAndIdAsync(owner, id);
+            }
+        }
+    }
+}
+
---
+++ src/ToDoListAPI/Data_Connect/DBUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace ToDoListAPI.Data_Connect
+{
+    public class DBUtility
+    {
+
+        public SqlConnection CreateConnection()
+        {
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Server=tcp:apiservices20160330040829dbserver.database.windows.net,1433;Database=jqdentaldb;User ID=dbuser@apiservices20160330040829dbserver;Password=Team6password;Encrypt=True;TrustServerCertificate=False;Connection Timeout=3;";
+            return myConnection;
+        }
+    }
+}
